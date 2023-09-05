cc86059ec084594951717bcdac9ab5ebc2033a52
Chang_Hyeon <eeooo3@naver.com>
2023-09-05 16:15:51 +0900

    [ADD] 새로운 풀이 추가

--- 05_string/Back_1152.cpp
+++ 05_string/Back_1152.cpp
@@ -31,3 +31,26 @@ int main(int argc, char argv[]) {
 //즉, 공백을 포함한 문장을 저장할 수는 없다
 //getlin(cin,str) : 공백 문자를 포함해서 문장을
 //저장할 수 있다.
+
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main(int argc, char const* argv[]) {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	string str;
+//	getline(cin, str);
+//	int cnt = 1;
+//	for (int i = 0; i < str.length(); i++) {
+//		if (str[i] == ' ')
+//			cnt++;
+//	}
+//	if (str[0] == ' ')
+//		cnt--;
+//	if (str[str.length() - 1] == ' ')
+//		cnt--;
+//	cout << cnt;
+//	return 0;
+//}
